afea7c08c339cded8a5e38f49e7017cb7c77aee3
hilmanrdn <hilman@hilman.com>
2019-04-01 08:18:48 +0800

    schedule a post

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,6 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-cd public
 # Add changes to git.
 git add .
 
@@ -19,6 +14,3 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-
-# Come Back up to the Project Root
-cd ..
